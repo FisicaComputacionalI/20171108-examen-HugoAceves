b294a59eeb53e17992e844fb66b89631ace9feec
HugoAceves <31254628+HugoAceves@users.noreply.github.com>
2017-11-10 13:59:17 -0600

    Add files via upload
    Se comienza el programa y se da un x=1 mientras equis sea menor o igul a cinco, entonces, si x%3==, tendremos x=x+2

---
+++ di.cc
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+  int x;
+  x=1;
+  while(x<=5){
+    if(x%3==0){
+      x=x+2;}
+    else{
+      cout<<"x="<<x<<endl;
+      x=x+2;}
+  }
+  while(x>5){
+    cout<<"x="<<x<<endl;
+    cin>>x;
+  }
+
+  return 0;
+}
